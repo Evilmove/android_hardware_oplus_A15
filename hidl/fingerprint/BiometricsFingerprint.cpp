d3feb5cbc494fd7e1c7e2056eb1dc9c8548622df
yograjfire18 <yograjfire18@gmail.com>
2024-05-24 21:53:28 +0530

    hidl: fingerprint: set dimlayer_hbm to 1 on touch fixes flikr when unlocking using udfps reference: https://github.com/RealmeGTNeo3T-Devs/android_hardware_oplus/commit/ca800e046e921b0428ff71b10875716507f20d39

--- hidl/fingerprint/BiometricsFingerprint.cpp
+++ hidl/fingerprint/BiometricsFingerprint.cpp
@@ -100,7 +100,7 @@ Return<void> BiometricsFingerprint::onFingerDown(uint32_t x, uint32_t y, float m
 Return<void> BiometricsFingerprint::onFingerUp() {
     setFpPress(0);
     if (!this->isEnrolling) {
-        setDimlayerHbm(0);
+        setDimlayerHbm(1);
     }
     return mOplusBiometricsFingerprint->onFingerUp();
 }
@@ -130,7 +130,7 @@ Return<void> BiometricsFingerprint::onError(uint64_t deviceId, FingerprintError 
                                             int32_t vendorCode) {
     setFpPress(0);
     if (!this->isEnrolling) {
-        setDimlayerHbm(0);
+        setDimlayerHbm(1);
     }
     return mClientCallback->onError(deviceId, error, vendorCode);
 }
